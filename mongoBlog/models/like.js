67e3e70db16180610b1ad595711ee4f7949d6d6f
Jason Ehrlicher <jehrlich@uno.edu>
2024-01-31 17:05:47 -0600

    change post and user to postID and user ID for like routes

--- mongoBlog/models/like.js
+++ mongoBlog/models/like.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const likeSchema = new mongoose.Schema({
-    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    createdAt: { type: Date, default: Date.now }
+    postID: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    timestamp: { type: Date, default: Date.now }
 });
 
 // This ensures that a user can only like a post once
-likeSchema.index({ post: 1, user: 1 }, { unique: true });
+likeSchema.index({ postID: 1, userID: 1 }, { unique: true });
 
 module.exports = mongoose.model("Like", likeSchema)
